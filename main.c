4d23dbe48c9e7d06dd3e42895edc4923b9b5074c
Dominik Strebel (raspi) <dominik.strebel@gmail.com>
2016-11-06 13:14:39 +0100

    Variable Buffersize

--- main.c
+++ main.c
@@ -26,94 +26,98 @@
  * 
  */
 static bool endFlag = false;
-static int tid = 0;
 
-
-void handle_exit(){
+void handle_exit() {
     printf("Ok, stopping.......\n");
-    endFlag=true;
+    endFlag = true;
 }
 
-void print_usage(){
+void print_usage() {
     printf("Welcome to Raspberry Pi Stresstest\n");
     printf("This Program executes several threads and moves data from\n");
     printf("/dev/zero to /dev/null to generate some stress\n");
-    printf("Please use stress -t [Number of threads]\n");
-    exit (EXIT_FAILURE);
+    printf("Please use stress -t [Number of threads] -b [size of Read/Write Buffer\n");
+    exit(EXIT_FAILURE);
 }
 
-
-void *workerThread(){
-    ssize_t bytesRead, bytesWritten;
-    char buffer[4096];
-    int in_fd,out_fd;
+void *workerThread(void *buffersize) {
+    ssize_t bytesRead, bytesWritten;;
+    char *buffer = calloc(*(int*) buffersize, sizeof(char));
+    //char buffer[4096];
+    int in_fd, out_fd;
     ssize_t readBytes, writtenBytes;
-    printf("Created thread: %d\n", ++tid);
     in_fd = open("/dev/zero", O_RDONLY);
-    if (in_fd<0){
+    if (in_fd < 0) {
         printf("Error opening /dev/zero as Input. Aborting...");
         exit(EXIT_FAILURE);
     }
     out_fd = open("/dev/null", O_WRONLY);
-    if (out_fd<0){
+    if (out_fd < 0) {
         printf("Error opening /dev/null as Output. Aborting...");
         exit(EXIT_FAILURE);
     }
-    while(endFlag==false){
-        readBytes = read(in_fd, &buffer, sizeof(buffer));
-        if (readBytes != sizeof(buffer)){
+    while (endFlag == false) {
+        readBytes = read(in_fd, &buffer, sizeof (buffer));
+        if (readBytes != sizeof (buffer)) {
             printf("Some reading error happened, exiting.....");
             exit(EXIT_FAILURE);
         }
-        writtenBytes = write(out_fd, &buffer, sizeof(buffer));
-        if (writtenBytes != sizeof(buffer)){
+        writtenBytes = write(out_fd, &buffer, sizeof (buffer));
+        if (writtenBytes != sizeof (buffer)) {
             printf("Some wiritng error happened, exiting......");
             exit(EXIT_FAILURE);
         }
     }
     close(in_fd);
     close(out_fd);
-    
+
 }
 
 int main(int argc, char** argv) {
     pthread_t *threads;
-    int terr=0;
-    int c, num_threads;
+    int terr = 0;
+    int c, num_threads, buffersize;
     char *c_threads = NULL;
-    while ((c = getopt(argc, argv, "t:")) != -1) {
+    char *c_buffersize = NULL;
+    while ((c = getopt(argc, argv, "t:b:")) != -1) {
         switch (c) {
             case 't':
                 c_threads = optarg;
                 break;
-            default: print_usage();       
+            case 'b':
+                c_buffersize = optarg;
+                break;
+            default: print_usage();
         }
     }
-    if (c_threads==NULL){
+    if (c_threads == NULL || c_buffersize == NULL) {
         print_usage();
     }
-     if (signal(SIGINT, handle_exit) == SIG_ERR){
-         printf("Can't catch SIGINT\n");
-         exit(EXIT_FAILURE);
-     }
+    if (signal(SIGINT, handle_exit) == SIG_ERR) {
+        printf("Can't catch SIGINT\n");
+        exit(EXIT_FAILURE);
+    }
     num_threads = atoi(c_threads);
-    threads = calloc(num_threads, sizeof(pthread_t));
-    for (int i=0; i<num_threads; i++){
-        terr = pthread_create(&threads[i], NULL, workerThread, NULL);
-        if (terr){
-            printf("Couldn't create thread %d, exiting.....", i);
+    buffersize = atoi(c_buffersize);
+    threads = calloc(num_threads, sizeof (pthread_t));
+    for (int i = 0; i < num_threads; i++) {
+        terr = pthread_create(&threads[i], NULL, workerThread, &buffersize);
+        printf("Starting thread %d\n", i);
+        fflush(stdout);
+        if (terr) {
+            printf("Couldn't create thread %d, exiting.....", i + 1);
             exit(EXIT_FAILURE);
         }
     }
-    for (int i=0; i<num_threads; i++){
+    for (int i = 0; i < num_threads; i++) {
         terr = pthread_join(threads[i], NULL);
-        if (terr){
+        if (terr) {
             printf("Couldn't join thread %d, aborting....", i);
             exit(EXIT_FAILURE);
         }
     }
     free(threads);
     printf("All threads stopped\n");
-    return (EXIT_SUCCESS); 
+    return (EXIT_SUCCESS);
 }
 
